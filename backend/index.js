70730c6454cbd31e09274192b5b383ff7e8f923c
Chavva Sai Soujanya <chavva.sai@accolite.com>
2023-08-09 16:42:16 +0530

    sample login and register

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,113 @@
-const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const express = require('express');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
+const mysql = require("mysql2/promise");
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
+const { GraphQLString } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Accolite@4477",
+  database: "online_shopping"
+});
+
 const app = express();
-const { graphqlHTTP } = require("express-graphql");
+app.use(cors());
 
-const {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-} = require("graphql");  // Fixed typo: changed 'reauired' to 'require'
+const typeDefs = gql`
+  type User {
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({  // Fixed typo: changed 'qyery' to 'query'
-    name: "OnlineShoppingWebsite",  // Changed name to a valid GraphQL name
-    fields: {
-      message: {
-        type: GraphQLString,
-        resolve: () => "Hello",
-      },
+    username: String
+    email: String
+    password: String
+    user_address: String
+  }
+
+  type Query {
+    users: [User]
+  }
+
+  type Mutation {
+    registerUser(username: String!, email: String!, password: String!, user_address: String!): User
+    loginUser(email: String!, password: String!): String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      const connection = await pool.getConnection();
+      try {
+        const [rows] = await connection.execute("SELECT * FROM users");
+        return rows;
+      } finally {
+        connection.release();
+      }
+    }
+  },
+  Mutation: {
+    registerUser: async (_, { username, email, password, user_address }) => {
+      const connection = await pool.getConnection();
+      try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await connection.execute(
+          "INSERT INTO users (username, email, password, user_address) VALUES (?, ?, ?, ?)",
+          [username, email, hashedPassword, user_address]
+        );
+        return { username, email, user_address };
+      } finally {
+        connection.release();
+      }
     },
-  }),
+    loginUser: async (_, { email, password }) => {
+      const connection = await pool.getConnection();
+      try {
+        const [rows] = await connection.execute("SELECT * FROM users WHERE email = ?", [email]);
+        if (rows.length === 0) {
+          throw new Error('User not found');
+        }
+
+        const user = rows[0];
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+          throw new Error('Invalid password');
+        }
+
+        const token = jwt.sign({ userId: user.user_id }, 'your-secret-key', { expiresIn: '1h' });
+        return token;
+
+      } finally {
+        connection.release();
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground
+  ]
 });
 
-app.use("/graphql", graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
+async function startServer() {
+  await server.start();
 
-app.listen(8081, () => {
-  console.log("Server is running on port 8081");
-});
+  server.applyMiddleware({ app });
+
+  const PORT = process.env.PORT || 4000;
+  const serverInstance = app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+  serverInstance.on('error', (error) => {
+    console.error('Server error:', error);
+  });
+}
+
+startServer();
