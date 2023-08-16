9b4c42337334be7742e57feec03dfe03477995e4
Chavva Sai Soujanya <chavva.sai@accolite.com>
2023-08-16 12:34:08 +0530

    server.js

--- backend/index.js
+++ backend/index.js
@@ -27,15 +27,54 @@ const typeDefs = gql`
     user_address: String
   }
 
+  
+  type Product {
+    product_id: Int!
+    product_name: String
+    product_price: Float
+    product_description: String
+    image: String
+  }
+  type CartItem {
+    user_id: Int!
+    product_id: Int!
+    quantity: Int!
+    order_id: Int
+    product: Product
+  }
+  type Order {
+    order_id: Int
+    user_id: Int
+    order_status: String
+    order_address: String
+    order_total: Float
+  }
+  
   type Query {
     users: [User]
+    products: [Product]
+    getCartItems(user_id: Int!): [CartItem]
+    getCartTotal(user_id: Int!): Float 
+    getUser(user_id: Int!): User
+    getOrdersByUserId(user_id: Int!): [Order]  
+    
+    
   }
-
   type Mutation {
     registerUser(username: String!, email: String!, password: String!, user_address: String!): User
-    loginUser(email: String!, password: String!): String
+    loginUser(email: String!, password: String!): String!
+    addToCart(user_id: Int!, product_id: Int!, quantity: Int!): String
+    placeOrder(
+      user_id: Int!,
+      order_status: String!,
+      order_address: String!, 
+      order_total: Float!
+    ): Order
+    deleteCartItem(user_id: Int!, product_id: Int!): String
   }
 `;
+
+
 
 const resolvers = {
   Query: {
@@ -47,7 +86,94 @@ const resolvers = {
       } finally {
         connection.release();
       }
-    }
+    },
+    
+
+    
+    products: async () => {
+      const connection = await pool.getConnection();
+      try {
+        const [rows] = await connection.execute("SELECT * FROM products");
+        return rows;
+      } finally {
+        connection.release();
+      }
+    },
+
+
+    
+
+
+    getCartTotal: async (_, { user_id }) => {
+      const connection = await pool.getConnection();
+      try {
+        
+        
+        const [rows] = await connection.execute(
+          "SELECT c.quantity, p.product_price FROM cart c JOIN products p ON c.product_id = p.product_id WHERE c.user_id = ? AND c.order_id IS NULL",
+          [user_id]
+        );
+
+        if(rows.length === 0){
+          return 0;
+        }
+
+        const cartTotal = rows.reduce((total, row) => {
+          return total + row.quantity * row.product_price;
+        }, 0);
+        
+
+        return cartTotal;
+      } finally {
+        connection.release();
+      }
+    },
+    getCartItems: async (_, { user_id }) => {
+      const connection = await pool.getConnection();
+      try {
+        
+        const [rows] = await connection.execute(
+          "SELECT c.user_id, c.product_id, c.quantity, p.* FROM cart c JOIN products p ON c.product_id = p.product_id WHERE c.user_id = ? AND c.order_id IS NULL",
+          [user_id]
+        );
+        
+        return rows;
+      } finally {
+        connection.release();
+      }
+    },
+    
+
+    getUser: async (_, { user_id }) => {
+      const connection = await pool.getConnection();
+      try {
+        
+        const [rows] = await connection.execute(
+          "SELECT user_id, username, email, user_address from users WHERE user_id = ?", [user_id],
+          [user_id]
+        );
+        
+        return rows[0];
+      } finally {
+        connection.release();
+      }
+    },
+
+    getOrdersByUserId: async (_, { user_id }) => {
+      const connection = await pool.getConnection();
+      try {
+        const [rows] = await connection.execute(
+          'SELECT * FROM orders WHERE user_id = ?',
+          [user_id]
+        );
+
+        
+        return rows;
+      } finally {
+        connection.release();
+      }
+    },
+    
   },
   Mutation: {
     registerUser: async (_, { username, email, password, user_address }) => {
@@ -77,15 +203,108 @@ const resolvers = {
         if (!isPasswordValid) {
           throw new Error('Invalid password');
         }
-
-        const token = jwt.sign({ userId: user.user_id }, 'your-secret-key', { expiresIn: '1h' });
-        return token;
+        const secretKey = process.env.JWT_SECRET_KEY || 'default-secret-key';
+        console.log(secretKey)
+        const token = jwt.sign({ user_id: user.user_id,username:user.username }, secretKey, { expiresIn: '24h' });
+        return  token;
 
       } finally {
         connection.release();
       }
-    }
-  }
+    },
+    addToCart: async (_, { user_id, product_id, quantity }) => {
+      const connection = await pool.getConnection();
+      try {
+        
+        const [existingItem] = await connection.execute(
+          "SELECT * FROM cart WHERE user_id = ? AND product_id = ? AND order_id IS NULL",
+          [user_id, product_id]
+        );
+
+        if (existingItem.length > 0) {
+          await connection.execute(
+            "UPDATE cart SET quantity = quantity + ? WHERE user_id = ? AND product_id = ?",
+            [quantity, user_id, product_id]
+          );
+        } 
+        else {
+          
+          await connection.execute(
+            "INSERT INTO cart (user_id, product_id, quantity) VALUES (?, ?, ?)",
+            [user_id, product_id, quantity]
+          );
+        }
+
+        return 'Product added to cart';
+      } finally {
+        connection.release();
+      }
+    
+    },
+    
+    
+    placeOrder: async (_, {user_id,
+      order_status,
+      order_address, 
+      order_total},{pool}) => {
+      try {
+        const connection = await pool.getConnection();
+        const [result] = await connection.execute(
+          'INSERT INTO orders (user_id, order_status, order_address, order_total) VALUES (?, ?, ?, ?)',
+          [user_id, order_status, order_address, order_total]
+        );
+
+        const order_id = result.insertId;
+
+        const [result1] = await connection.execute(
+          'UPDATE cart SET order_id =  ? WHERE user_id = ?',
+          [order_id, user_id]
+        );
+        
+
+        connection.release();
+        return {
+          user_id,
+          order_id,
+          order_status,
+          order_address,
+          order_total
+        };
+      } catch (error) {
+        throw new UserInputError('Failed to place order');
+      }
+    },
+    deleteCartItem: async (_, { user_id, product_id }) => {
+      const connection = await pool.getConnection();
+      try {
+        await connection.execute(
+          'DELETE FROM cart WHERE user_id = ? AND product_id = ?',
+          [user_id, product_id]
+        );
+        return 'Cart item deleted successfully';
+      } catch (error) {
+        throw new Error('Failed to delete cart item');
+      } finally {
+        connection.release();
+      }
+    },
+    
+    
+  },
+  CartItem: {
+    product: async (parent, _, { pool }) => {
+      const connection = await pool.getConnection();
+      try {
+        const [rows] = await connection.execute('SELECT * FROM products WHERE product_id = ? ', [parent.product_id]);
+        return rows[0];
+      } finally {
+        connection.release();
+      }
+    },
+  },
+  
+    
+  
 };
 
 const server = new ApolloServer({
@@ -93,7 +312,10 @@ const server = new ApolloServer({
   resolvers,
   plugins: [
     ApolloServerPluginLandingPageGraphQLPlayground
-  ]
+  ],
+  context:{
+    pool
+  }
 });
 
 async function startServer() {
